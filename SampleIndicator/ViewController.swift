3a1cdba0dd7d007901ef0cf937dfa8b1f3dc3737
TakahashiIkki <new_ittyandesu@outlook.jp>
2016-09-21 08:37:17 +0900

    Indicator の処理 の作成

--- SampleIndicator/ViewController.swift
+++ SampleIndicator/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var myActivityIndicator: UIActivityIndicatorView!
+    @IBOutlet private var myButton: UIButton!
+    
+    // NavigationBar の 右位置に ActivityIndicator を作成
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        myActivityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+        myActivityIndicator.hidesWhenStopped = true
+        self.onClickMyButton(self)
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: myActivityIndicator)
     }
 
+    // ボタンクリック時の挙動
+    @IBAction func onClickMyButton(_ sender: AnyObject) {
+        if myActivityIndicator.isAnimating {
+            myActivityIndicator.stopAnimating()
+            myButton.setTitle("Start", for: .normal)
+        }
+        else {
+            myActivityIndicator.startAnimating()
+            myButton.setTitle("Stop", for: .normal)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
